a1760bd8acc7bf7b09de817e84e16017d1acacc3
Tom <thomasagaskill@gmail.com>
2023-08-27 20:43:24 +0100

    refactor: remove redundant useState from trpc Provider

--- src/app/_trpc/Provider.tsx
+++ src/app/_trpc/Provider.tsx
@@ -2,20 +2,18 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { httpBatchLink } from "@trpc/client";
-import { useState, type PropsWithChildren } from "react";
+import { type PropsWithChildren } from "react";
 import { trpc } from "./client";
 
 export default function Provider({ children }: PropsWithChildren) {
-  const [queryClient] = useState(() => new QueryClient({}));
-  const [trpcClient] = useState(() =>
-    trpc.createClient({
-      links: [
-        httpBatchLink({
-          url: "http://localhost:3000/api/trpc",
-        }),
-      ],
-    })
-  );
+  const queryClient = new QueryClient();
+  const trpcClient = trpc.createClient({
+    links: [
+      httpBatchLink({
+        url: "http://localhost:3000/api/trpc",
+      }),
+    ],
+  });
 
   return (
     <trpc.Provider client={trpcClient} queryClient={queryClient}>
